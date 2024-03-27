dc71d40fd8002fe6420a11df1341e3257a1788db
Jared Chase <djaredchase@gmail.com>
2024-03-27 12:12:31 -0400

    styled the hero section

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -7,12 +7,12 @@ export const HeroSection = () => {
         <div className='hero-section-container'>
             <div className='hero-section-text'>
                 <h1>Little lemon</h1>
-                <h3>Chicago</h3>
+                <h2>Chicago</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dapibus eleifend euismod.</p>
                 <Button variant='secondary'>Reserve a table</Button>
             </div>
             <div className='hero-section-img'>
-                <img src='' alt='restaurant food'/>
+                <img src={FoodImage} alt='restaurant food'/>
             </div>
         </div>
     )
